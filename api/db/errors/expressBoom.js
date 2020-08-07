3ff53580a1196be4f1e555c18a33f1398a9f35e8
Stephen <sun.stephen.oo@gmail.com>
2020-08-07 14:31:39 -0400

    add reusabe Boom error handle for Express web routing services

--- api/db/errors/expressBoom.js
+++ api/db/errors/expressBoom.js
@@ -1,3 +1,13 @@
 // File: db/errors/expressBoom.js
 // Date: 8/7/2020
-// Note: Final Steps to Production
+// Note: Final Steps to Production: 
+
+/// RENDERING - ROUTING - RESOURCING - DATA I/Os
+
+module.exports = function(err, req, res, next) {
+    res.set(err.output.headers);
+    res.status(error.output.statusCode);
+    res.json(err.output.payload);
+}
+
+// eof
